8b3781d61385c20a7bc7c8cd3b871e27178d3505
juhi priya <juhivi@gmail.com>
2020-12-05 10:33:33 +0100

    1.commit:Form+alapveto funkciok

---
+++ FCIIN5_Beadando/FCIIN5_Beadando/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace FCIIN5_Beadando
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Kereses = new System.Windows.Forms.Button();
+            this.keresesinput = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.SuspendLayout();
+            // 
+            // Kereses
+            // 
+            this.Kereses.Location = new System.Drawing.Point(338, 9);
+            this.Kereses.Name = "Kereses";
+            this.Kereses.Size = new System.Drawing.Size(110, 23);
+            this.Kereses.TabIndex = 0;
+            this.Kereses.Text = "Keresés";
+            this.Kereses.UseVisualStyleBackColor = true;
+            this.Kereses.Click += new System.EventHandler(this.Kereses_Click);
+            // 
+            // keresesinput
+            // 
+            this.keresesinput.Location = new System.Drawing.Point(12, 12);
+            this.keresesinput.Name = "keresesinput";
+            this.keresesinput.Size = new System.Drawing.Size(305, 20);
+            this.keresesinput.TabIndex = 1;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 38);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(436, 400);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.keresesinput);
+            this.Controls.Add(this.Kereses);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Kereses;
+        private System.Windows.Forms.TextBox keresesinput;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ FCIIN5_Beadando/FCIIN5_Beadando/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace FCIIN5_Beadando
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Kereses_Click(object sender, EventArgs e)
+        {
+            if (keresesinput.Text != null && keresesinput.Text.Length >= 3)
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load("Termekek.xml");
+            }
+            else
+            {
+                MessageBox.Show("Nem megfelelő beviteli paraméterek");
+                keresesinput.Text = string.Empty;
+                keresesinput.Focus();
+            }
+        }
+    }
+}
